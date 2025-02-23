6bebbf0d02135360f493c1385e955ac21f62b578
Feggand <44234765+Feggand@users.noreply.github.com>
2025-02-23 22:50:49 +0100

    ^^

--- main.js
+++ main.js
@@ -5,32 +5,16 @@ var memoryManager = require('memoryManager');
 module.exports.loop = function () {
     console.log('Main loop running');
     memoryManager.initializeMemory();
-
-    if (Game.time % 100 === 0) {
-        for (let name in Memory.creeps) {
-            if (!Game.creeps[name]) {
-                delete Memory.creeps[name];
-            }
-        }
-    }
+    console.log('Memory initialized');
 
     for (let roomName in Game.rooms) {
         let room = Game.rooms[roomName];
-        console.log(`Managing room: ${roomName}, isMyRoom: ${Memory.rooms[roomName]?.isMyRoom}`);
-        if (roomManager && typeof roomManager.manageRoom === 'function') {
-            roomManager.manageRoom(room);
-        } else {
-            console.log(`Error: roomManager or manageRoom not defined for ${roomName}`);
-        }
+        console.log(`Room: ${roomName}`);
+        // Kommentiere den problematischen Aufruf vorerst aus
+        // roomManager.manageRoom(room);
+        console.log('Room processed');
     }
 
     creepManager.runCreeps();
-
-    for (let roomName in Game.rooms) {
-        let room = Game.rooms[roomName];
-        if (room.controller && room.controller.my) {
-            let towers = room.find(FIND_MY_STRUCTURES, { filter: s => s.structureType === STRUCTURE_TOWER });
-            towers.forEach(tower => require('tower').run(tower));
-        }
-    }
+    console.log('Creeps run');
 };
